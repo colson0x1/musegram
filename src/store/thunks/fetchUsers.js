df7529d77566a19f2ba999bcc4d8ef5c4bb4213b
Colson <colsoncalifornication@gmail.com>
2023-07-21 11:52:50 +0545

    create a temporary delay helper function that tests loading state

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,17 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
+  // development test
+  await pause(1000);
+
   return response.data;
 });
+
+// development test
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration)
+  })
+}
 
 export { fetchUsers };
