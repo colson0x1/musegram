046dd52d1d928b8774861e0afa0ce6446e940c7e
Colson <colsoncalifornication@gmail.com>
2023-07-21 19:55:34 +0545

    add delete and the loading spinner

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,7 +1,23 @@
+import { GoTrash } from 'react-icons/go';
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/use-thunks';
+
 function UsersListItem({ user }) {
+  const [doRemoveUser, isLoadingDeletingUser, removeUserError] =
+    useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
+        <Button loading={isLoadingDeletingUser} onClick={handleClick}>
+          <GoTrash />
+        </Button>
+        {removeUserError && <div>Error deleting user.</div>}
         {user.name}
       </div>
     </div>
