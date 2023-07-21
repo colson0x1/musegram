a32b22c189acb6db8a9907270e745a7ccdb29d2d
Colson <colsoncalifornication@gmail.com>
2023-07-21 20:08:41 +0545

    resolve deleting user

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -14,11 +14,13 @@ function UsersListItem({ user }) {
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
-        <Button loading={isLoadingDeletingUser} onClick={handleClick}>
-          <GoTrash />
-        </Button>
-        {removeUserError && <div>Error deleting user.</div>}
-        {user.name}
+        <div className="flex flex-row items-center justify-between">
+          <Button className="mr-3" loading={isLoadingDeletingUser} onClick={handleClick}>
+            <GoTrash />
+          </Button>
+          {removeUserError && <div>Error deleting user.</div>}
+          {user.name}
+        </div>
       </div>
     </div>
   );
