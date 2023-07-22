3ab543d9ff54447f3db90b817892fef672980932
Colson <colsoncalifornication@gmail.com>
2023-07-22 07:59:10 +0545

    add the albums listing

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -3,6 +3,7 @@ import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/use-thunks';
 import ExpandablePanel from './ExpandablePanel';
+import AlbumsList from './AlbumsList';
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoadingDeletingUser, removeUserError] =
@@ -26,7 +27,11 @@ function UsersListItem({ user }) {
     </>
   );
 
-  return <ExpandablePanel header={header}>CONTENT!!</ExpandablePanel>;
+  return (
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
+  );
 }
 
 export default UsersListItem;
